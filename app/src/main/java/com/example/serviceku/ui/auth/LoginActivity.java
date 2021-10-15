d27656871e6b37c35e51954d4289bb95a7eee465
Gustana <gustana.satiawan@gmail.com>
2021-10-15 19:35:03 +0700

    add mapbox

--- app/src/main/java/com/example/serviceku/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/serviceku/ui/auth/LoginActivity.java
@@ -10,9 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.serviceku.R;
 import com.example.serviceku.databinding.ActivityLoginBinding;
+import com.example.serviceku.helper.SPManager;
 import com.example.serviceku.room.DBHolder;
 import com.example.serviceku.room.entity.UserEntity;
-import com.example.serviceku.helper.SPManager;
 import com.example.serviceku.ui.admin.HomeAdminActivity;
 import com.example.serviceku.ui.user.HomeUserActivity;
 import com.example.serviceku.util.LoginUtil;
@@ -41,8 +41,12 @@ public class LoginActivity extends AppCompatActivity implements LoginUtil {
     @Override
     public void login() {
         String username = binding.edtUsername.getText().toString();
+        String password = binding.edtUsername.getText().toString();
 
-        String password = binding.edtPassword.getText().toString();
+        if (username.equals("") || password.equals("")) {
+            Toast.makeText(this, "Input Kosong", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         if (username.equals(getString(R.string.admin)) && password.equals(getString(R.string.admin))) {
             Log.i(TAG, "onPostExecute: login as admin");
@@ -63,19 +67,23 @@ public class LoginActivity extends AppCompatActivity implements LoginUtil {
                 @Override
                 protected void onPostExecute(UserEntity userEntity) {
                     super.onPostExecute(userEntity);
-                    if (password.equals(userEntity.getPassword())) {
-                        Log.i(TAG, "onPostExecute: login as User");
-
-                        userId = dbHolder.getAppDB().userDao().getIdUser(username);
-
-                        Log.i(TAG, "onPostExecute: userId: " + userId);
-                        spManager.setLogin(userId, username, password, SPManager.LEVEL_USER);
-
-                        Intent i = new Intent(getApplicationContext(), HomeUserActivity.class);
-                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(i);
+                    if (userEntity == null) {
+                        Toast.makeText(getApplicationContext(), "Belum terdaftar", Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_SHORT).show();
+                        if (password.equals(userEntity.getPassword())) {
+                            Log.i(TAG, "onPostExecute: login as User");
+
+                            userId = dbHolder.getAppDB().userDao().getIdUser(username);
+
+                            Log.i(TAG, "onPostExecute: userId: " + userId);
+                            spManager.setLogin(userId, username, password, SPManager.LEVEL_USER);
+
+                            Intent i = new Intent(getApplicationContext(), HomeUserActivity.class);
+                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
             }
